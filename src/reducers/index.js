03b076f854eb390fcc17fbc55e3945b9fc4cedf2
github username <github email address>
2020-09-11 12:58:30 -0400

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,27 +1,7 @@
 import { combineReducers } from 'redux';
 import quotes from './quotes';
-import uuid from 'uuid';
 
 export default combineReducers({
   quotes
 });
 
-// const rootReducer = combineReducers({
-//   quotes: quotesReducer
-// });
- 
-// export default rootReducer;
-
-function quotesReducer(state = [], action) {
-  switch (action.type) {
-    case "ADD_QUOTE":
-      return [...state, action.quote];
- 
-    // case "REMOVE_BOOK":
-    //   idx = state.findIndex(book => book.id  === action.id)
-    //   return [...state.slice(0, idx), ...state.slice(idx + 1)];
- 
-    default:
-      return state;
-  }
-}
