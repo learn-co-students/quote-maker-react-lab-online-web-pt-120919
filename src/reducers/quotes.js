eea419cb4f766f4b366a6aaa225a1abf0348b4be
RebeccaHA <rebeccahunt034@gmail.com>
2020-09-21 19:51:16 +0100

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
-}
+  let idx;
+  let quote;
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case "UPVOTE_QUOTE":
+      return state;
+
+    case "DOWNVOTE_QUOTE":
+      return state;
+
+    default:
+      return state;
+  }
+};
