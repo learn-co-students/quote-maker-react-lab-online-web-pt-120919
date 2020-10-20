37fadcfadc9861fc22de6f8c7743f324495bae50
Candice Peters <candicelizabethpeters@gmail.com>
2020-10-20 03:17:02 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
+switch(action.type){
+  case "ADD_QUOTE":
+  return [...state, action.quote]
+
+  case "REMOVE_QUOTE":
+  return state.filter(quote => quote.id !== action.quoteId)
+
+  case "UPVOTE_QUOTE":
+  let newState = state.map(quote => {
+    if(quote.id === action.quoteId){
+      return {...quote, votes: ++quote.votes}
+    } else {
+      return quote
+    }
+  })
+  return newState;
+
+  case "DOWNVOTE_QUOTE":
+  let downState = state.map(quote => {
+    if(quote.id === action.quoteId && quote.votes > 0){
+      return {...quote, votes: --quote.votes}
+    } else {
+      return quote
+    }
+  })
+  return downState;
+
+  default:
   return state;
+  }
 }
